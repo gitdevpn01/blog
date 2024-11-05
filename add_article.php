4b1479f58eb8eebd168c95020f1aa9588f6ba26b
formk_ceppic <formk@kermed.fr>
2024-11-05 16:48:14 +0100

    Ajout insertion des données du formulaire dans la base de donnée.

--- add_article.php
+++ add_article.php
@@ -1,3 +1,30 @@
 <?php
 
-echo 'Test';
+var_dump("debug");
+die();
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+
+try {
+    $connexion = new PDO("mysql:host=localhost;dbname=blog_db", "root", "");
+} catch (PDOException $error)
+{
+    echo "Erreur s'est produit lors de la connexion à votre base de donnée : " . $error->getMessage();
+}
+
+
+$connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$stm = $connexion->prepare("INSERT INTO article (title,content) VALUES (:title, :content) ");
+
+$stm->bindParam(':title', $title);
+$stm->bindParam(':content', $content);
+
+if($stm->execute()):
+    header("Location: index.php");
+else:
+    echo "Erreur lors de l'ajout d'un article !!!!!!!!!!!!";
+endif;
+
+$connexion = null;
